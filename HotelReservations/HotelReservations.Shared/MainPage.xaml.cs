d96e21a6595c2e8f3458f757b24c119a0c914f3b
Clifford Choi <29491530+CliffShow@users.noreply.github.com>
2022-11-09 09:03:00 -0800

    Completed navigation

--- HotelReservations/HotelReservations.Shared/MainPage.xaml.cs
+++ HotelReservations/HotelReservations.Shared/MainPage.xaml.cs
@@ -9,9 +9,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml.Media.Animation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,20 +24,63 @@ namespace HotelReservations
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private NavigationViewItem _lastItem;
+        
         public MainPage()
         {
             this.InitializeComponent();
         }
-        
-        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
         {
-            var item = args.InvokedItemContainer;
-            switch (item.Name)
+            
+        }
+        
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            var item = args.InvokedItemContainer as NavigationViewItem;
+            if (item == null || item == _lastItem) return;
+
+            var clickedView = item.Tag?.ToString(); //?? "SettingsView";
+            
+            if (!NavigateToView(clickedView)) return;
+            
+            _lastItem = item;
+        }
+
+        private bool NavigateToView(string clickedView)
+        {
+            var view = Assembly.GetExecutingAssembly().GetType($"HotelReservations.{clickedView}");
+
+            if (string.IsNullOrWhiteSpace(clickedView) || view == null) return false;
+            
+            ContentFrame.Navigate(view, null, new EntranceNavigationTransitionInfo());
+
+            return true;
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            foreach (NavigationViewItemBase item in NavView.MenuItems)
             {
-                case "CreateReservation":
-                    ContentFrame.Navigate(typeof(ReservationPage));
+                if (item is NavigationViewItem && item.Tag.ToString() == "HomeView")
+                {
+                    NavView.SelectedItem = item;
                     break;
+                }
             }
+
+            ContentFrame.Navigate(typeof(HotelReservations.HomeView));
+        }
+
+        private void NavView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            
+        }
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (ContentFrame.CanGoBack) ContentFrame.GoBack();
         }
     }
 }
